6f3ad9a621e1fdd81187820c1ea971aab4109d37
DamienShahan <Damien.Shahan@gmail.com>
2019-07-12 17:57:40 +0200

    Added castbar and manabar

--- src/Components/main.tsx
+++ src/Components/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Raidframes from './raidframesComponent/RaidframesComponent';
+import Castbar from './castbarComponent/CastbarComponent';
 
 import "./main.css"
 
@@ -14,32 +15,35 @@ class HealbotSite extends React.Component {
 
     render() {
         return (
-            <div className="boss1">
-                <div className="boss-area">
-                    <div className="boss b1">
-                        <div className="current_life">
+            <div className="encounter">
+                <div className="top-area">
+                    <div className="boss-area">
+                        <div className="boss b1">
+                            <div className="current_life">
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="raid-area">
-                    <div className="raid">
-
-                        <Raidframes/>
-
-                        <div className="clearer">
+                <div className="middle-area">
+                    <div className="raid-area">
+                        <div className="raid">
+                            <Raidframes/>
+                            <div className="clearer">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="castbar_area">
+                        <Castbar/>
+                    </div>
+                    <div className="manabar_area">
+                        <div className="manabar">
                         </div>
                     </div>
                 </div>
-                <div className="manabar_area">
-                    <div className="manabar">
+                <div className="bottom-area">
+                    <div className="cd_area">
                     </div>
-                </div>
-                <div className="castbar_area">
-                    <div className="castbar">
-                    </div>
-                </div>
-                <div className="cd_area">
-                </div>
+                </div>  
             </div>
 
         )
