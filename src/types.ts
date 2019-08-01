9dae9242eb42d1bf1c9c9774c539ebd907c94347
Relmnicoder <Relmnight@Gmail.com>
2019-08-01 11:27:15 +0200

    added types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface ClassRole {
+    dps?: string,
+    heal?: string,
+    tank?: string
+}
+
+export interface Raider {
+    classRole: "dps" | "tank" | "heal"
+    alive: boolean,
+    RaiderId: number,
+    maxHp: number,
+    currentHp: number,
+    buffs: ((arg0: number) => number)[],
+    debuffs: ((arg0: number) => number)[],
+
+}
+
+export interface Raid {
+    Raiders: Raider[],
+    Spells: []
+    Player: {}
+}
