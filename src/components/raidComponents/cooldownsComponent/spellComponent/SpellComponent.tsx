ece29843026afaed444959f4d5fbadaaf1963b39
DamienShahan <Damien.Shahan@gmail.com>
2019-08-07 17:42:55 +0200

    Fixed numbers, but it still doesn't show.

--- src/components/raidComponents/cooldownsComponent/spellComponent/SpellComponent.tsx
+++ src/components/raidComponents/cooldownsComponent/spellComponent/SpellComponent.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import spell1 from '../../../../img/spell1.gif'
-import spell2 from '../../../../img/spell2.gif'
-import spell3 from '../../../../img/spell3.gif'
-import spell4 from '../../../../img/spell4.gif'
-import spell5 from '../../../../img/spell5.gif'
-import spell6 from '../../../../img/spell6.gif'
-import spell7 from '../../../../img/spell7.gif'
-import spell8 from '../../../../img/spell8.gif'
-import spell9 from '../../../../img/spell9.gif'
-import spell10 from '../../../../img/spell10.gif'
+import spell0 from '../../../../img/spell1.gif'
+import spell1 from '../../../../img/spell2.gif'
+import spell2 from '../../../../img/spell3.gif'
+import spell3 from '../../../../img/spell4.gif'
+import spell4 from '../../../../img/spell5.gif'
+import spell5 from '../../../../img/spell6.gif'
+import spell6 from '../../../../img/spell7.gif'
+import spell7 from '../../../../img/spell8.gif'
+import spell8 from '../../../../img/spell9.gif'
+import spell9 from '../../../../img/spell10.gif'
 
 import "./spell.css"
 
@@ -20,7 +20,6 @@ interface IRaiderProps {
 class Spell extends React.Component<IRaiderProps, any> {
 
     spellSrc = "spell" + this.props.identifier
-
     // constructor(props){
     //     super(props)
     // }
