09dc1b2fd73d6d3b16c10a159d249bb13f930859
Dustin Shahan <dshahan@ict.ag>
2019-11-30 19:36:29 +0100

    added raid reducer options

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -11,25 +11,49 @@ const raidReducer = (raidState: Raider[] = [], action: any) => {
         case "DAMAGE":
             return raidState.map((raider, i) => {
                 if (i === action.payload.id) {
-                    return {...raider, currentHp: raider.currentHp - action.payload.damageAmount}
+                    return { ...raider, currentHp: raider.currentHp - action.payload.damageAmount }
                 }
                 return raider
             })
+        case "HEALING":
+            return raidState.map((raider, i) => {
+                if (i === action.payload.id) {
+                    return { ...raider, currentHp: raider.currentHp - action.payload.healAmount }
+                }
+                return raider
+            })
+        case "BUFF":
+            return raidState.map((raider, i) => {
+                if (i === action.payload.id) {
+                    return { ...raider, buffs: [...raider.buffs, action.payload.buff] }
+                }
+                return raider
+            })
+        case "DEBUFF":
+            return raidState.map((raider, i) => {
+                if (i === action.payload.id) {
+                    return { ...raider, debuffs: [...raider.debuffs, action.payload.debuff] }
+                }
+                return raider
+            })
+
+        case "TOGGLE_LIFE":
+            return raidState.map((raider, i) => {
+                if (i === action.payload.id) {
+                    return { ...raider, alive: !raider.alive }
+                }
+                return raider
+            })
+
+        /*TODO: Check if i can somehow make:
+         return raidState.map((raider, i) => {
+            if (i === action.payload.id) {}
+          }
+        go away since its tedious and sort of dumb.  */
         default:
             return raidState
     }
 }
-
-// const raidHealthReducer = (state: StateStore, action: any) => {
-//     switch (action.type) {
-//         case "REDUCE_HEALTH":
-//             return [
-//                 state.raiders[5].currentHp - 5,
-//             ]
-//         default:
-//             return state
-//     }
-// }
 
 export default combineReducers(
     {
