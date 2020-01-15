30d20ebdd82f0f214ff153e2fdb259914968a567
Dustin Shahan <dshahan@ict.ag>
2020-01-15 15:43:07 +0100

    updated typescript

--- src/components/raidComponents/enemyComponents/Enemies.tsx
+++ src/components/raidComponents/enemyComponents/Enemies.tsx
@@ -1,53 +1,63 @@
-import React, { useEffect } from 'react'
-import Boss from './boss/Boss'
-import Boss1Config from '../../../raids/Ini-1/Boss1Config.json'
+import React, { useEffect, useState } from "react";
+import Boss from "./boss/Boss";
+import Boss1Config from "../../../raids/Ini-1/Boss1Config.json";
 
-import "./enemies.css"
-import { useDispatch } from 'react-redux'
-import { damage } from '../../../actions'
-import { Raider } from '../../../types'
-
+import "./enemies.css";
+import { useDispatch } from "react-redux";
+import { damage } from "../../../actions";
+import { Raider } from "../../../types";
 
 interface enemyProps {
-    raiders: Raider[]
+  raiders: Raider[];
 }
 
 const Enemies: React.FC<enemyProps> = props => {
-    const dispatch = useDispatch()
+  const [RandomInt, setRandomInt] = useState(0);
+  const dispatch = useDispatch();
 
-    let totalBosses = Boss1Config.enemies.boss
-    let Bosses = []
+  let totalBosses = Boss1Config.enemies.boss;
+  let Bosses = [];
 
-    for (let i = 0; i < totalBosses; i++) {
-        Bosses.push(
-            <Boss key={i} identifier={i.toFixed()}></Boss>
-        )
+  for (let i = 0; i < totalBosses; i++) {
+    Bosses.push(<Boss key={i} identifier={i.toFixed()}></Boss>);
+  }
+
+  let LiveRaiders = props.raiders.filter(raider => {
+    return raider.alive;
+  });
+
+  console.log(LiveRaiders[Math.floor(Math.random() * LiveRaiders.length)]);
+  let currentRaider =
+    LiveRaiders[Math.floor(Math.random() * LiveRaiders.length)];
+  console.log(currentRaider?.RaiderId);
+
+  let fightStart = () => dispatch(damage(35, Math.floor(Math.random() * 10)));
+  //   let fightStart = () =>
+  //     dispatch(
+  //       damage(
+  //         35,
+  //         LiveRaiders
+  //           ? LiveRaiders[Math.floor(Math.random() * LiveRaiders.length)].RaiderId
+  //           : 10
+  //       )
+  //     );
+
+  useEffect(() => {
+    if (LiveRaiders) {
+      const interval = setInterval(() => {
+        fightStart();
+      }, 1000);
+      return () => clearInterval(interval);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    let LiveRaiders = props.raiders.map(raider => {
-        if (raider.alive)
-            return raider.RaiderId
-        return
-    });
-    console.log(LiveRaiders)
-
-    let fightStart = () => dispatch(damage(35, Math.floor(Math.random() * 10)))
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fightStart()
-        }, 1000);
-        return () => clearInterval(interval);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-
-    return (
-        <div className="bosses noselect">
-            {Bosses}
-            <div className="clearer"></div>
-        </div>
-    )
-}
+  return (
+    <div className="bosses noselect">
+      {Bosses}
+      <div className="clearer"></div>
+    </div>
+  );
+};
 
 export default Enemies;
