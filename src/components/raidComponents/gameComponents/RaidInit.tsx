bb9b0e6b0bc6d1beb8808cbed4d13e2ef48a04b2
Dustin Shahan <dshahan@ict.ag>
2020-02-17 13:25:21 +0100

    Damage continues and doesnt reset. No more check to see when everyone is dead

--- src/components/raidComponents/gameComponents/RaidInit.tsx
+++ src/components/raidComponents/gameComponents/RaidInit.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { StateStore } from "../../../types";
@@ -8,29 +8,27 @@ import { damage } from "../../../actions";
 function RaidInit() {
   const raiders = useSelector((state: StateStore) => state.raiders);
 
-  const [LiveRaiders, setLiveRaiders] = useState();
+  const LiveRaiders = useRef<any>([{}]);
   const dispatch = useDispatch();
 
   //set array with living raiders
   useEffect(() => {
-    setLiveRaiders(
-      raiders.filter(raider => {
-        return raider.alive;
-      })
-    );
-
+    LiveRaiders.current = raiders.filter(raider => {
+      return raider.alive;
+    });
   }, [raiders]);
 
-//rerender on click happens because of LiveRaiders
+  //rerender on click happens because of LiveRaiders
   useEffect(() => {
-    if (LiveRaiders?.length > 0) {
       const interval = setInterval(() => {
-        const randomRaider = LiveRaiders[Math.floor(Math.random() * LiveRaiders.length)]?.RaiderId
-        dispatch(damage(35, randomRaider))
+        const randomRaider =
+          LiveRaiders.current[
+            Math.floor(Math.random() * LiveRaiders.current.length)
+          ]?.RaiderId;
+        dispatch(damage(150, randomRaider));
       }, 1000);
-      return () => clearInterval(interval)
-    }
-  }, [LiveRaiders, dispatch] );
+      return () => clearInterval(interval);
+  }, [LiveRaiders, dispatch]);
 
   return <></>;
 }
